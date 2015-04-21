0663ef8f9e7df54bd15fe12fe159f260b5dbb523
Charles <zhangchaoxu@gmail.com>
2015-04-21 09:17:40 +0800

    add google analysis

--- app/src/main/java/com/lean56/moneykiller/MyApplication.java
+++ app/src/main/java/com/lean56/moneykiller/MyApplication.java
@@ -1,9 +1,46 @@
 package com.lean56.moneykiller;
 
 import android.app.Application;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Logger;
+import com.google.android.gms.analytics.Tracker;
+
+import java.util.HashMap;
 
 /**
  * App Application
  */
 public class MyApplication extends Application {
+
+    public MyApplication() {
+        super();
+    }
+
+    // [+] Google Analytics
+    // The following line should be changed to include the correct property id.
+    private static final String PROPERTY_ID = "UA-XXXXX-Y";
+
+    public enum TrackerName {
+        APP_TRACKER, // Tracker used only in this app.
+        GLOBAL_TRACKER, // Tracker used by all the apps from a company. eg: roll-up tracking.
+        ECOMMERCE_TRACKER, // Tracker used by all ecommerce transactions from a company.
+    }
+
+    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+
+    synchronized Tracker getTracker(TrackerName trackerId) {
+        if (!mTrackers.containsKey(trackerId)) {
+
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            analytics.getLogger().setLogLevel(Logger.LogLevel.VERBOSE);
+            Tracker t = (trackerId == TrackerName.APP_TRACKER) ? analytics.newTracker(PROPERTY_ID)
+                    : (trackerId == TrackerName.GLOBAL_TRACKER) ? analytics.newTracker(
+                    R.xml.global_tracker)
+                    : analytics.newTracker(R.xml.ecommerce_tracker);
+            t.enableAdvertisingIdCollection(true);
+            mTrackers.put(trackerId, t);
+        }
+        return mTrackers.get(trackerId);
+    }
+    // [-] Google Analytics
 }
