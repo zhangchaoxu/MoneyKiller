d6d04ddadfac557d68e2a59ae489eaed8a094685
Charles <zhangchaoxu@gmail.com>
2015-07-08 09:49:09 +0800

    move junit folder

---
+++ app/src/test/java/com/lean56/moneykiller/test/JunitUtils.java
@@ -0,0 +1,14 @@
+package com.lean56.moneykiller.test;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Charles on 7/7/2015.
+ */
+public class JunitUtils {
+    @Test
+    public void add() {
+        Assert.assertTrue(3 == 1 + 2);
+    }
+}
