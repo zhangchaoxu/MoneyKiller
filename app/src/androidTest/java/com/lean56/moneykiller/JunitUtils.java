3ffb844b5d5867ac9809bff66470bd4cfd127465
Charles <zhangchaoxu@gmail.com>
2015-07-08 08:16:41 +0800

    add junit test

---
+++ app/src/androidTest/java/com/lean56/moneykiller/JunitUtils.java
@@ -0,0 +1,10 @@
+package com.lean56.moneykiller;
+
+/**
+ * Created by Charles on 7/7/2015.
+ */
+public class JunitUtils {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
